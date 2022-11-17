b517a4337df36038c56b60cbd33a1df9e42b0b99
Cbutler1 <collin.p.butler1@gmail.com>
2022-11-17 16:54:52 -0500

    Event management not posting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventFormComponent } from './event-form/event-form.component';
 
 import { EventsComponent } from './events/events.component';
 import { UserManagementComponent } from './user-management/user-management.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'events', component: EventsComponent },
   { path: '', component: EventsComponent },
   { path: 'users', component: UserManagementComponent },
+  { path: 'event-form', component: EventFormComponent}
 ];
 
 @NgModule({
