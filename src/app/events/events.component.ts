a066a6fc851bf40f6ec59b9f6d880da58fad71c7
Troy Wilson <102485470+Buggerdo@users.noreply.github.com>
2022-11-17 07:11:22 -0500

    Added tag showing user joined

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -11,6 +11,8 @@ export class EventsComponent implements OnInit {
   currentUser: User = {} as User;
   userParticipations: ThingToDo[] = [];
 
+  userParticipationsIds: Number[] = [];
+
   events: ThingToDo[] = [];
 
   constructor(private _service: EventSystemService) {}
@@ -18,23 +20,29 @@ export class EventsComponent implements OnInit {
   ngOnInit(): void {
     this.loadUsers();
   }
-
+  
   selectUser(user: User) {
     this.currentUser = user;
     this.loadUsersEvents(this.currentUser);
-    console.table(this.userParticipations);
+    
+    //console.table(this.userParticipations);
   }
 
   loadUsers = (): void => {
     this._service.getEvents().subscribe((data: ThingToDo[]) => {
       this.events = data;
-      console.log(this.events);
+      //console.log(this.events);
+      //console.log("Events loaded");     
+      //console.log(this.userParticipations);
     });
   };
 
   loadUsersEvents = (user: User): void => {
     this._service.getEventsByUser(user.id).subscribe((data: ThingToDo[]) => {
       this.userParticipations = data;
+      this.getParticipationIds();
+      //console.log("Events loaded");     
+      //console.log(this.userParticipations);
     });
   };
 
@@ -44,4 +52,13 @@ export class EventsComponent implements OnInit {
       this.userParticipations.push(event);
     });
   }
+
+
+    getParticipationIds = () => {
+    let ids: Number[] = [];
+    this.userParticipations.forEach((participation) => {
+      ids.push(participation.id);
+      this.userParticipationsIds = ids;
+    });
+}
 }
